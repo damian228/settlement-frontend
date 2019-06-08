30e7b6932d8e984cb1b0224666c2d22aff1ff826
Damian Kisielinski <damian.kisielinski@gmail.com>
2019-06-08 17:56:07 +0200

    manager info fix sort

--- src/app/manager/info/info.service.ts
+++ src/app/manager/info/info.service.ts
@@ -9,14 +9,7 @@ export class InfoService {
   constructor(private httpClient: HttpClient) {}
 
   getEmployees(): Observable<EmployeeInfoDTO[]> {
-    //return this.httpClient.get('/manager/info/employees').pipe(map((res: EmployeeInfoDTO[]) => res));
-    return of([
-      { employeeId: 'admin', hours: 45, remuneration: 450 },
-      { employeeId: 'admin1', hours: 45, remuneration: 950 },
-      { employeeId: 'admin2', hours: 55, remuneration: 450 },
-      { employeeId: 'admin3', hours: 75, remuneration: 850 },
-      { employeeId: 'admin4', hours: 95, remuneration: 4150 }
-    ]);
+    return this.httpClient.get('/manager/info/employees').pipe(map((res: EmployeeInfoDTO[]) => res));
   }
 
   getSummary(): Observable<SummaryDTO> {
